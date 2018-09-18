4cb43cb04582dc97530134091d0a4fd7eeb9b8e2
Speedphoenix <leonardo.jeanteur@gmail.com>
2018-09-18 22:00:44 +0200

    moved all from subfolder of Stiek2Bellum

---
+++ Behaviours/Animator/Composing/Shadow.cpp
@@ -0,0 +1,15 @@
+#include "Shadow.h"
+
+//using namespace std;
+//
+//Shadow::Shadow()
+//    :m_Original(nullptr), m_canEvolve(false)
+//{
+//    //ctor
+//}
+//
+//Shadow::~Shadow()
+//{
+//    //dtor
+//}
+
---
+++ Behaviours/Behaviour.cpp
@@ -0,0 +1,25 @@
+#include "Behaviour.h"
+#include "GameContainer.h"
+#include "GameObject.h"
+
+Behaviour::Behaviour(GameObject* attachTo)
+    :m_attachedObject(attachTo)
+{
+    GameContainer* instance = GameContainer::instance();
+
+    if (!instance)
+        throw "No instance of GameContainer";
+
+    m_containerIterator = instance->addBehaviour(this);
+}
+
+Behaviour::~Behaviour()
+{
+    GameContainer* instance = GameContainer::instance();
+
+    if (instance)
+    {
+        instance->removeBehaviour(m_containerIterator);
+    }
+}
+
